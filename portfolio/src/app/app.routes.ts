a41946457b6ed566af2d56416f24f07c5837f650
khanyansa <khanyizwane.nsa@gmail.com>
2024-09-11 07:13:32 +0200

    app.routes.ts update

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,15 +1,4 @@
-// import { Routes } from '@angular/router';
-// import { HomeComponent } from './home/home.component';
-// import { AboutComponent } from './about/about.component';
-// import { ContactComponent } from './contact/contact.component';
 
-// export const routes: Routes = [
-//   { path: '', redirectTo: 'home', pathMatch: 'full' },
-//   { path: 'home', component: HomeComponent },
-//   { path: 'about', component: AboutComponent },
-//   { path: 'contact', component: ContactComponent },
-//   { path: '**', redirectTo: 'home' }  // wildcard route for handling 404s
-// ];
 
 
 import { NgModule } from '@angular/core';
